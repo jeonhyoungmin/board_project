99b3f8ac4796d7cc420f851820d5c79047bd343c
jeonhyoungmin <jhmin7509@gamil.com>
2023-05-25 16:56:22 +0900

    Service 객체에서는 Dao 만 사용하도록 수정. 새로운 게시물 등록 추가. 게시물 삭제 추가. 게시물 수정 추가.

--- src/main/java/com/surup/board/service/BoardService.java
+++ src/main/java/com/surup/board/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.surup.board.service;
 
 import com.surup.board.dao.BoardDao;
+import com.surup.board.dao.UserDao;
 import com.surup.board.domain.BoardDto;
 import com.surup.board.domain.SearchCondition;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ import java.util.List;
 public class BoardService { // 새 글 쓰기, 수정하기, 삭제하기
     @Autowired
     BoardDao boardDao;
+
+    @Autowired
+    UserDao userDao;
 
     // SELECT
     public List<BoardDto> getBoardAll() throws Exception {
@@ -25,22 +29,37 @@ public class BoardService { // 새 글 쓰기, 수정하기, 삭제하기
         return boardDao.selectPage(sc);
     }
 
+    public BoardDto getBoardOne(Integer bno) throws Exception {
+        return boardDao.selectOne(bno);
+    }
+
+    public String  getWriterId(Integer bno) throws Exception {
+        BoardDto boardDto = boardDao.selectOne(bno);
+        String writer = boardDto.getWriter();
+        return userDao.selectNickname(writer).getId();
+    }
+
 
     // INSERT
     public int registerBoard(BoardDto boardDto) throws Exception {
         return boardDao.insert(boardDto);
     }
 
+    public int registerBoard(String id, BoardDto boardDto) throws Exception {
+        String writer = userDao.selectId(id).getNickname();
+        boardDto.setWriter(writer);
+        return boardDao.insert(boardDto);
+    }
+
 
     // DELETE
-//    public int deleteAccount(String id) throws Exception {
-////        throw new Exception();
-//        return userDao.deleteUser(id);
-//    }
+    public int deleteBoardOne(Integer bno) throws Exception {
+        return boardDao.delete(bno);
+    }
 
 
     // UPDATE
-//    public int updatePwd(UserDto userDto) throws Exception {
-//        return userDao.updatePwd(userDto);
-//    }
+    public int updateBoardOne(BoardDto boardDto) throws Exception {
+        return boardDao.updateBoard(boardDto);
+    }
 }
